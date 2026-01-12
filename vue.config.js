0116fc67c097f18c20f052a8fe9101573195f765
dovutuan <>
2026-01-12 17:58:37 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const publicPath = process.env.VUE_APP_PUBLIC_PATH ?? '/';
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://dovutuan.github.io/wedding/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? publicPath : '/',
 })
